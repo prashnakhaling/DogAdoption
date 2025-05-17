108616ea88eb1e546a8b6177bd51865aa0b816f8
prashnakhaling <prashna.krai@gmail.com>
2025-05-17 22:48:49 +0545

    header created
    added logo with navigation

---
+++ homepage.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<title>Dog Adoption</title>
+<link rel="stylesheet" href="adoptionstyle.css">
+<script>
+    function toggleSidebar() {
+        const sidebar = document.getElementById('sidebar');
+        sidebar.classList.toggle('show');
+    }
+
+    function closeSidebar() {
+        document.getElementById('sidebar').classList.remove('show');
+        document.getElementById('overlay').classList.remove('show');
+    }
+</script>
+<div class="dogadotion-main">
+    <header>
+        <nav class="header-nav">
+            <a href="">
+                <img src="images/logo-42.png" alt="littlepaws" class="header-logo"></img>
+            </a>
+            <a href="#dogs">Dogs for Adoption</a>
+            <a href="#about">About Us</a>
+        </nav>
+
+        <div class="sidebar-toggle" onclick="toggleSidebar()">☰</div>
+
+    </header>
+    <aside class="sidebar" id="sidebar">
+        <div class="close-btn" onclick="closeSidebar()">✖</div>
+        <button>Login</button>
+        <button>Sign Up</button>
+        <button>Logout</button>
+    </aside>
+</div>
+
+
+</html>
